a40d449d0cb784dd2df69d875961a529f323d0cf
fayis <fayisfazz418@gmail.com>
2025-08-07 14:49:56 +0530

    fff

---
+++ Loops/Advanced Level Tasks Loops/anagram.py
@@ -0,0 +1,26 @@
+# str1 = input("Enter first string: ").lower()
+# str2 = input("Enter second string: ").lower()
+
+# for char in str1:
+# count1 = 0
+# count2 = 0
+# for c in str1:
+#    if c == char:
+#          count1 += 1
+# for c in str2:
+#    if c == char:
+#          count2 += 1
+# if count1 != count2:
+#    found = False
+#    break
+# if found:
+# print("Strings are anagrams.")
+# else:
+# print("Strings are not anagrams.")
+
+# num=input("Enter 1st string :")
+# num1=input("Enter 2nd string :")
+# if sorted(num)==sorted(num1):
+#    print("its anagram")
+# else:
+#    print("not anagram")
